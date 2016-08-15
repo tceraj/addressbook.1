1715c9686223be86359f88ae1919ab9c62ea8baa
Prelang Builder <builder@prelang.com>
2016-08-15 22:51:48 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Car' to allow for create, update

--- app/admin/car.rb
+++ app/admin/car.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Car do
+  permit_params :model, :make, :registered_until
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
